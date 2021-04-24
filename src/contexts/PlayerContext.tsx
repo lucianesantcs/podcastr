45b18787b2373d3484191ee3f9e3a91a8c11097a
Luciane Santos <lucianesantcs@gmail.com>
2021-04-24 10:34:37 -0300

    playercontext refatorado, passado funcionalidades do app para o proprio contexto

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useState, ReactNode } from "react";
+
+type Episode = {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+};
+
+type PlayerContextData = {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
+  play: (episode: Episode) => void;
+  setPlayingState: (state) => void;
+  togglePlay: () => void;
+};
+
+type PlayerContextProviderProps = {
+  children: ReactNode;
+};
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+}
